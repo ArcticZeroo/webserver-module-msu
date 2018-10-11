21162c96419eefc2251ce95bc87678789750f78d
Spencer <spencer@frozor.io>
2018-10-11 16:17:29 -0400

    Use cors and non-react port for webserver testing

--- test/local-testing/webserver.js
+++ test/local-testing/webserver.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 const RootModule = require('../../module');
 
@@ -12,4 +14,4 @@ new RootModule({
 
 app.use((req, res) => res.status(404).send('404 Not Found'));
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(4000, () => console.log('Listening on port 4000'));
