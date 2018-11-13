ed9c52128e146926c26bdd76455aa3253c22b8b8
Spencer <spencer@frozor.io>
2018-11-13 14:52:51 -0500

    Add NodeUtil for setIntervalImmediate

---
+++ lib/src/util/NodeUtil.ts
@@ -0,0 +1,12 @@
+import Duration from '@arcticzeroo/duration';
+
+export default class NodeUtil {
+    static setIntervalImmediate(callback: () => any, ms: Duration|number) {
+        if (ms instanceof Duration) {
+            ms = ms.inMilliseconds;
+        }
+
+        callback();
+        setInterval(callback, ms);
+    }
+}
